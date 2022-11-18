88c38085d037ca891820ff49c3aecf2a0bdb8a68
Marco Franco <piratafranco20@gmail.com>
2022-11-18 02:02:37 -0800

    Cambios al programa 1, de acuerdo a instrucciones

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,9 +1,13 @@
 def isOdd():
+    print("Bienvenido a par o impar, ingresa un numero entre 1 y 1000 y descubre si es par o impar")
     number = int(input("Ingresa un numero: "))
-    if number % 2 == 0:
-        print("El numero es par")
+    if number > 0 and number < 1001:
+        if number % 2 == 0:
+            print("El numero es par")
+        else:
+            print("El numero es impar")
     else:
-        print("El numero es impar")
+        print("Ingresa un numero que cumpla con el rango indicado")
 
 
 isOdd()
